a4ccac5b5e07fac0c3ce878311e2c3e5ba35d7e3
Aaron Tetrick <atetrick14102@gmail.com>
2022-09-23 01:57:02 -0400

    Add JavaScipt logic for selecting Option 1 and add in content to Option 1 dropdown

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-document.addEventListener('click', )
+document.addEventListener('click', e => {
+    console.log(e.target.parentElement)
+    const isDropdownBtn = e.target.classList.contains('option');
+    const currentDropdown = e.target.parentElement
+    if(!isDropdownBtn && currentDropdown != null) return
+
+
+    if (isDropdownBtn) {
+        currentDropdown.classList.toggle('active');
+
+
+    }
+
+    document.querySelectorAll('.active').forEach(dropdown => {
+        console.log(dropdown);
+        if (dropdown === currentDropdown) return
+        dropdown.classList.remove('active');
+    })
+})
